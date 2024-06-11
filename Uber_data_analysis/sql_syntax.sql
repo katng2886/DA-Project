9466d8a59fb7be323a122465ff3d4bd31d1185c5
Katie Nguyen <116457246+katng2886@users.noreply.github.com>
2024-06-10 21:52:02 -0400

    Update sql_syntax.sql

--- Uber_data_analysis/sql_syntax.sql
+++ Uber_data_analysis/sql_syntax.sql
@@ -2,8 +2,8 @@
 SELECT VENDORID, AVG(FARE_AMOUNT) Avg_fare_amount, SUM(fare_amount) Total_fare_amount FROM `datasetid.dataname.fact_table` GROUP BY VENDORID;
 
 -- Find the average amount of tip categorized by payment type:
-select b.payment_type_name, round(AVG(a.tip_amount),2) Avg_tip_amount from `datasetid.dataname.fact_table` a join `hopeful-text-419519.uber_data_engineering.payment_type_dim` b on a.payment_type_id = b.payment_type_id group by b.payment_type_name;
+select b.payment_type_name, round(AVG(a.tip_amount),2) Avg_tip_amount from `datasetid.dataname.fact_table` a join `{Project_name}.uber_data_engineering.payment_type_dim` b on a.payment_type_id = b.payment_type_id group by b.payment_type_name;
 
 -- Find the total amount of tip categorized by payment type:
-select b.payment_type_name, round(SUM(a.tip_amount),2) total_tip_amount from `datasetid.dataname.fact_table` a join `hopeful-text-419519.uber_data_engineering.payment_type_dim` b on a.payment_type_id = b.payment_type_id group by b.payment_type_name;
+select b.payment_type_name, round(SUM(a.tip_amount),2) total_tip_amount from `datasetid.dataname.fact_table` a join `{project_name}.uber_data_engineering.payment_type_dim` b on a.payment_type_id = b.payment_type_id group by b.payment_type_name;
 
